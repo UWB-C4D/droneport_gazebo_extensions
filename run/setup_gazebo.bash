6f7f3b967ec6b92e4730dc62097cd0e718f6d34a
Petr Neduchal <neduchal@gmail.com>
2022-11-18 09:25:10 +0100

    Added new sitl_gazebo modelst path

--- run/setup_gazebo.bash
+++ run/setup_gazebo.bash
@@ -19,7 +19,7 @@ BUILD_DIR=$2
 source /usr/share/gazebo/setup.sh; 
 source /usr/share/mavlink_sitl_gazebo/setup.sh;
 export GAZEBO_PLUGIN_PATH=/usr/lib/x86_64-linux-gnu/gazebo-9/plugins:/usr/lib/x86_64-linux-gnu/gazebo-11/plugins:$SRC_DIR/../gazebo_simulation/build:$BUILD_DIR/build_gazebo:$GAZEBO_PLUGIN_PATH;
-export GAZEBO_MODEL_PATH=$SRC_DIR/../gazebo_simulation/models:$SRC_DIR/Tools/sitl_gazebo/models:$GAZEBO_MODEL_PATH;
+export GAZEBO_MODEL_PATH=$SRC_DIR/../gazebo_simulation/models:$SRC_DIR/Tools/sitl_gazebo/models:$SRC_DIR/Tools/simulation/gazebo/sitl_gazebo/models:$GAZEBO_MODEL_PATH;
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$BUILD_DIR/build_gazebo
 
 export PX4_HOME_LAT=49.7252222
